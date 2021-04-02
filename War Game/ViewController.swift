9a4a1c8e7fb8b512966933ebe918c66e9d16fc78
Kyle Sherrington <kylejsherrington@gmail.com>
2021-04-01 19:05:23 -0700

    Initial Commit

---
+++ War Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  War Game
+//
+//  Created by Kyle Sherrington on 2021-04-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
